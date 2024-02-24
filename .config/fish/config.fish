9c3979ae6eec0021aa688b2dbb1ab9c941847a2a
TallestLegacy <tallestlegacy@gmail.com>
2024-02-24 14:55:11 +0300

    adjusted kitty and alacritty fonts, plus minor hyprlan]d configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ end
 set -U fish_user_paths $HOME/go/bin/ $fish_user_paths
 
 # Java Home
-set -Ux JAVA_HOME /usr/lib/jvm/java-11-openjdk
+set -Ux JAVA_HOME ~/.sdkman/candidates/java/17.0.10-ms/
 
 # Android Home
 set -Ux ANDROID_HOME /perm/Android/Sdk
@@ -16,6 +16,10 @@ set -U fish_user_paths $ANDROID_HOME/emulator $fish_user_paths
 set -U fish_user_paths $ANDROID_HOME/platform-tools $fish_user_paths
 set -U fish_user_paths $ANDROID_HOME/tools $fish_user_paths
 set -U fish_user_paths $ANDROID_HOME/tools-bin $fish_user_paths
+
+
+# flutter
+set -U fish_user_paths /perm/flutter/bin $fish_user_paths
 
 # add starfish prompt
 starship init fish | source
