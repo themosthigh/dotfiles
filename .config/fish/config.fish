969263c69e948d60ee0b4da84934e0709131f927
TallestLegacy <tallestlegacy@gmail.com>
2025-01-11 21:20:40 +0300

    progressive update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,23 @@ end
 alias vim="nvim"
 set -gx EDITOR nvim
 
+
+if uname = Darwin
+# Local bins
+    set -U fish_user_paths /opt/local/bin/ $fish_user_paths
+end
+
+# Homebrew
+    set -U fish_user_paths /opt/homebrew/bin/ $fish_user_paths
+
 # Go bin 
 set -U fish_user_paths $HOME/go/bin/ $fish_user_paths
+
+# Lando bin 
+set -U fish_user_paths $HOME/.lando/bin/ $fish_user_paths
+
+# Laravel composer
+set -U fish_user_paths $HOME/.composer/vendor/bin/ $fish_user_paths
 
 # Fly.io
 set -U fish_user_paths $HOME/.fly/bin/ $fish_user_paths
@@ -14,8 +29,13 @@ set -U fish_user_paths $HOME/.fly/bin/ $fish_user_paths
 # Java Home
 set -Ux JAVA_HOME ~/.sdkman/candidates/java/current/bin/
 
+switch (uname)
+  case Linux
 # Android Home
-set -Ux ANDROID_HOME /perm/Android/Sdk
+    set -Ux ANDROID_HOME /perm/Android/Sdk
+  case Darwin
+    set -Ux ANDROID_HOME $HOME/Library/Android/Sdk
+end
 
 # important paths
 set -U fish_user_paths $ANDROID_HOME/emulator $fish_user_paths
@@ -39,3 +59,6 @@ alias nvchad="NVIM_APPNAME=nvchad nvim"
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+export PATH="/Users/marvin/.lando/bin:$PATH"; #landopath
+
--- .wezterm.lua
+++ .wezterm.lua
@@ -1,72 +1,71 @@
 local wezterm = require("wezterm")
 
 local gruvbox_material_colors = {
-	-- Darker colors
-	bg0_hard = "#1d2021",
-	bg0 = "#282828",
-	bg1 = "#3c3836",
-	bg2 = "#504945",
-	bg3 = "#665c54",
-	bg4 = "#7c6f64",
+  -- Darker colors
+  bg0_hard = "#1d2021",
+  bg0 = "#282828",
+  bg1 = "#3c3836",
+  bg2 = "#504945",
+  bg3 = "#665c54",
+  bg4 = "#7c6f64",
 
-	gray = "#928374",
+  gray = "#928374",
 
-	fg0 = "#fbf1c7",
-	fg1 = "#ebdbb2",
-	fg2 = "#d5c4a1",
-	fg3 = "#bdae93",
-	fg4 = "#a89984",
+  fg0 = "#fbf1c7",
+  fg1 = "#ebdbb2",
+  fg2 = "#d5c4a1",
+  fg3 = "#bdae93",
+  fg4 = "#a89984",
 
-	-- Brighter colors
-	bg_green = "#b8bb26",
-	bg_yellow = "#fabd2f",
-	bg_blue = "#83a598",
-	bg_purple = "#d3869b",
-	bg_aqua = "#8ec07c",
-	bg_orange = "#fe8019",
-	bg_red = "#fb4934",
+  -- Brighter colors
+  bg_green = "#b8bb26",
+  bg_yellow = "#fabd2f",
+  bg_blue = "#83a598",
+  bg_purple = "#d3869b",
+  bg_aqua = "#8ec07c",
+  bg_orange = "#fe8019",
+  bg_red = "#fb4934",
 
-	fg_green = "#b8bb26",
-	fg_yellow = "#fabd2f",
-	fg_blue = "#83a598",
-	fg_purple = "#d3869b",
-	fg_aqua = "#8ec07c",
-	fg_orange = "#fe8019",
-	fg_red = "#fb4934",
+  fg_green = "#b8bb26",
+  fg_yellow = "#fabd2f",
+  fg_blue = "#83a598",
+  fg_purple = "#d3869b",
+  fg_aqua = "#8ec07c",
+  fg_orange = "#fe8019",
+  fg_red = "#fb4934",
 }
 
 local colors = {
-	transparent = "#00000000",
-	white = "#ffffff",
-	black = "#000000",
+  transparent = "#00000000",
+  white = "#ffffff",
+  black = "#000000",
 }
 
 for k, v in pairs(gruvbox_material_colors) do
-	colors[k] = v
+  colors[k] = v
 end
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Gruvbox Material (Gogh)"
-
+config.color_scheme = 'Kanagawa (Gogh)'
 -- fonts
 config.font = wezterm.font_with_fallback({
-	{
-		family = "MonaspiceAr Nerd Font",
-		italic = false,
-	},
+  {
+    family = "MonaspiceAr Nerd Font",
+    italic = false,
+  },
 })
 
 config.font_rules = {
-	{
-		italic = true,
-		font = wezterm.font_with_fallback({
-			{
-				family = "MonaspiceRn Nerd Font",
-				italic = true,
-			},
-		}),
-	},
+  {
+    italic = true,
+    font = wezterm.font_with_fallback({
+      {
+        family = "MonaspiceRn Nerd Font",
+        italic = true,
+      },
+    }),
+  },
 }
 
 config.font_size = 8
@@ -75,65 +74,66 @@ config.cell_width = 1
 
 --ligature settings
 config.harfbuzz_features = {
-	"ss01",
-	"ss02",
-	"ss03",
-	"ss04",
-	"ss05",
-	"ss06",
-	"ss07",
-	"ss08",
-	"calt",
-	"dlig",
+  "ss01",
+  "ss02",
+  "ss03",
+  "ss04",
+  "ss05",
+  "ss06",
+  "ss07",
+  "ss08",
+  "calt",
+  "dlig",
 }
 
-config.window_background_opacity = 0.99
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 5
 
 -- window frame
 config.window_frame = {
-	font = wezterm.font("JetBrains Mono", {
-		weight = "Bold",
-		italic = true,
-	}),
-	font_size = 9,
+  font = wezterm.font("JetBrains Mono", {
+    weight = "Bold",
+    italic = true,
+  }),
+  font_size = 9,
 
-	-- border
-	border_left_width = 1,
-	border_right_width = 1,
-	border_bottom_height = 1,
-	border_top_height = 1,
+  -- border
+  border_left_width = 1,
+  border_right_width = 1,
+  border_bottom_height = 1,
+  border_top_height = 1,
 
-	-- border color
-	border_left_color = "gray",
-	border_right_color = "gray",
-	border_bottom_color = "gray",
-	border_top_color = "gray",
+  -- border color
+  border_left_color = "gray",
+  border_right_color = "gray",
+  border_bottom_color = "gray",
+  border_top_color = "gray",
 
-	-- pane colors
-	inactive_titlebar_bg = colors.bg0,
-	active_titlebar_bg = colors.bg1,
-	inactive_titlebar_fg = colors.fg0,
-	active_titlebar_fg = colors.fg_green,
-	inactive_titlebar_border_bottom = colors.bg0,
-	active_titlebar_border_bottom = colors.bg1,
-	button_fg = colors.fg1,
-	button_bg = colors.bg1,
-	button_hover_fg = colors.bg_purple,
-	button_hover_bg = colors.bg1,
+  -- pane colors
+  inactive_titlebar_bg = colors.bg0,
+  active_titlebar_bg = colors.bg1,
+  inactive_titlebar_fg = colors.fg0,
+  active_titlebar_fg = colors.fg_green,
+  inactive_titlebar_border_bottom = colors.bg0,
+  active_titlebar_border_bottom = colors.bg1,
+  button_fg = colors.fg1,
+  button_bg = colors.bg1,
+  button_hover_fg = colors.bg_purple,
+  button_hover_bg = colors.bg1,
 }
 
 config.integrated_title_buttons = { "Close" }
 
 config.inactive_pane_hsb = {
-	saturation = 0.9,
-	brightness = 0.8,
+  saturation = 0.9,
+  brightness = 0.8,
 }
 
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 config.window_decorations = "RESIZE"
