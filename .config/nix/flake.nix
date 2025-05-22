3194ee7fafd7fed06e9879d8831f3dfabcbbaff5
TheMostHigh <tallestlegacy@gmail.com>
2025-05-22 15:15:13 +0300

    feat: nix flakes on macos

---
+++ .config/nix/flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "The Most High nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nix-darwin, nixpkgs }@inputs:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [
+          pkgs.neofetch
+
+          # pkgs.mkalias # TODO: add aliases for macos
+          # pkgs.ghostty # FIXME: package is currently broken
+
+          pkgs.nerd-fonts.monaspace
+          pkgs.nerd-fonts.fira-code
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
+      modules = [
+          configuration
+          ./modules/developer-tools.nix
+      ];
+    };
+  };
+}
---
+++ .config/nix/modules/developer-tools.nix
@@ -0,0 +1,63 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages =  with pkgs; [
+  # general tools 
+  git
+  gh
+  lazygit
+  ripgrep
+  android-tools
+
+  # editors
+  helix
+  neovim
+
+  gnumake
+    
+  # nix
+  nil
+
+  # lua
+  lua-language-server
+  stylua
+
+  # c/cpp
+  gcc
+  
+  # go
+  go
+  gopls
+  golines
+  gofumpt
+  goimports-reviser
+  gomodifytags
+
+  # js/ts
+  nodejs_20
+  nodePackages.pnpm
+  nodePackages.typescript-language-server
+  nodePackages.svelte-language-server
+  # nodePackages_latest.vue-language-server
+  nodePackages.prettier
+  prettierd
+  emmet-ls
+  eslint_d
+
+  # rust
+  rustup
+  rust-analyzer
+
+  # toml
+  taplo
+
+  # dart/flutter
+  dart
+  # flutter
+
+  # java
+  jdk11	
+
+  # zig
+  zig
+  ];
+}
