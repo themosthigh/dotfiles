fa2344eb393856e8dd34d6570fefc28c6c180d6e
TheMostHigh <tallestlegacy@gmail.com>
2025-06-02 15:11:28 +0300

    flake: updated nerd fonts to use homebrew

--- nix/modules/homebrew.nix
+++ nix/modules/homebrew.nix
@@ -1,7 +1,14 @@
 { ... }: {
   homebrew = {
     enable = true;
-    casks = [ "vagrant" "ghostty" "slack" "figma" "firefox" ];
+    casks = [
+      "vagrant"
+      "ghostty"
+      "slack"
+      "figma"
+      "firefox"
+      "font-monaspace-nerd-font"
+    ];
     onActivation.cleanup = "zap";
   };
 }
--- nix/modules/system-tools.nix
+++ nix/modules/system-tools.nix
@@ -4,11 +4,6 @@
 
     pkgs.mkalias # TODO: add aliases for macos
 
-    pkgs.nerd-fonts.monaspace
-    pkgs.nerd-fonts.fira-code
-    pkgs.monaspace
-    pkgs.cascadia-code
-
     pkgs.starship
     pkgs.stow
     pkgs.fzf
